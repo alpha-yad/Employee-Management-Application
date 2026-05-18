937204ac1768fdfa69fc24221a95bfea2e21382b
Ashish <97456465+alpha-yad@users.noreply.github.com>
2026-05-18 16:34:38 +0530

    Remove unused import statement

--- src/main/java/com/example/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/example/employee_management/EmployeeManagementApplication.java
@@ -1,5 +1,5 @@
 package com.example.employee_management;
-
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
